35a244106001b70bfe580683e5d7ca91a2f6c907
warleon <51892390+warleon@users.noreply.github.com>
2024-09-23 09:15:35 -0500

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -10,5 +10,11 @@ const browser = await puppeteer.launch();
 const page = await browser.newPage();
 
 await page.setContent(html, { waitUntil: "networkidle0" });
-await page.pdf({ path: "resume.pdf", format: "a4", printBackground: true });
+await page.pdf({
+  path: "resume.pdf",
+  printBackground: true,
+  width: "210mm", // Custom width equivalent to A4 width (8.27 inches)
+  height: `${await page.evaluate(() => document.body.scrollHeight)}px`, // Set height based on content
+  preferCSSPageSize: true, // Allow CSS to control the page size
+});
 await browser.close();
